4c66d45b5fbe3f4148f8d27793c684aec91db0f1
Vikram Goyal <vikramgoyalsg@gmail.com>
2025-01-19 10:01:46 +0800

    More stuff

--- src/pages/games/binarynumbers/binarynumbers.js
+++ src/pages/games/binarynumbers/binarynumbers.js
@@ -38,7 +38,7 @@ function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 async function clearBin() {
-    for (let i = 0; i < 5; i++) {
+    for (let i = 0; i < 3; i++) {
         console.log(`Waiting ${i} seconds...`);
         await sleep(i * 1000);
     }
@@ -53,23 +53,67 @@ for (var i = 0; i < 25; i++) {
     inputRows[i] = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
 }
 
-for (var i = 0; i < 25; i++) {
-    var rowInput = prompt();
-    inputRows[i] = rowInput.split(',').map(function(item) {
-        return parseInt(item);
-    });
-}
-
-async function enterBin() {
+// for (var i = 0; i < 25; i++) {
+//     var rowInput = prompt();
+//     inputRows[i] = rowInput.split("").map(function(item) {
+//         return parseInt(item);
+//     });
+// }
+function myFalse() {
+    return false;
+  }
+  
+async function inputBin() {
+    const waiter = await sleep(5000);
+    const myTimeout = setTimeout(() => false, 10000);
+    console.log(myTimeout);
     var i = 0;
-    for (let i = 0; i < 25; i++) {
-        console.log(`Waiting ${i} seconds...`);
-        await sleep(i * 1000);
+    do {
+        var rowInput = prompt("Enter row " + (i + 1));
+        if (i < 25) {
+            inputRows[i] = rowInput.split("").map(function(item) {
+                return parseInt(item);
+            });
+        }
+        i++;
+        console.log(myTimeout);
+    } while (myTimeout && i < 25);
+    console.log('Input Done');
+}
+
+inputBin();
+
+async function finish() {
+    await sleep(5000);
+    for (var i = 0; i < 25; i++) {
+        var incorrects = 0;
+        var lastRowPoints = 0;
+        if (inputRows[i] == [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]) {
+            break;
+        }
+        if (i < 24 && inputRows[i+1] != [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]) {
+            for (var j = 0; j < 30; j++) {
+                if (rows[i][j] != inputRows[i][j]) {
+                    incorrects++;
+                }
+            }
+            if (incorrects == 0) {
+                score += 30;
+            } else if (incorrects == 1) {
+                score += 15;
+            }
+        } else {
+            for (var j = 0; j < 30; j++) {
+                if (rows[i][j] == inputRows[i][j]) {
+                    lastRowPoints++;
+                } else {
+                    lastRowPoints = Math.ceil(lastRowPoints/2);
+                    break;
+                }
+            }
+            score += lastRowPoints;
+        }
     }
-    console.log('Done');
-    table.innerHTML = "";
+    document.getElementById("demo").innerHTML = "Your score is: " + score;
 }
-
-for (var i = 0; i < 25; i++) {
-
-}
+finish();
